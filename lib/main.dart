2636731db4d24d37d1ff2aa93b3c1d42bb99cbfb
wheeb2025 <omrwheeb@gmail.com>
2025-08-26 19:35:24 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text('نور عربي')),
+        body: const Center(child: Text('نسخة تجريبية - سورة الفاتحة فقط')),
+      ),
+    );
+  }
+}
